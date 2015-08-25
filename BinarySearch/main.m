804f8bc7cd71e714159b12c84ecd61a6597692af
Arsenyk Ustaris <arsenyku@users.noreply.github.com>
2015-08-25 13:28:39 -0700

    Initial Commit

---
+++ BinarySearch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BinarySearch
+//
+//  Created by asu on 2015-08-25.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
